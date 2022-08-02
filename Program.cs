6f5fdab81e1a98fb2c684c2916197f5fef3b0c5f
Muhammad Ishaque Junejo <ishaquejunejo33@gmail.com>
2022-08-02 13:59:29 +0500

    Added C# and C# Project files. Which later will be used for the Engine.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs;
+
+namespace Radius2D
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = 0;
+
+            for (var i = 0; i <= 50; i++)
+            {
+                num += i;
+                Console.WriteLine(i);
+            };
+
+            Console.WriteLine(num);
+        }
+    }
+}
