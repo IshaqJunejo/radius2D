9cbafc8f968c0640f1a13a44bdf3bc6e1f126710
Muhammad Ishaque Junejo <ishaquejunejo33@gmail.com>
2022-08-04 00:31:28 +0500

    Balls are now falling down

--- Particle.cs
+++ Particle.cs
@@ -16,5 +16,33 @@ namespace Radius2D
         {
             Raylib.DrawCircleV(pos, radius, Color.RAYWHITE);
         }
+
+        public void Update()
+        {
+            float gravity = 0.5f;
+            float terminalVel = 15.0f;
+
+            force.Y += gravity * mass;
+
+            vel += force / mass;
+
+            if (vel.X >= terminalVel)
+            {
+                vel.X = terminalVel;
+            }else if (vel.X < -terminalVel)
+            {
+                vel.X = -terminalVel;
+            };
+
+            if (vel.Y >= terminalVel)
+            {
+                vel.Y = terminalVel;
+            }else if (vel.Y < -terminalVel)
+            {
+                vel.Y = -terminalVel;
+            };
+
+            pos += vel;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,19 +13,19 @@ namespace Radius2D
 
             Raylib.InitWindow(Width, Height, "Physics Simulation");
 
-            int numOfCircles = 5;
+            int numOfCircles = 50;
             List<Particle> circles = new List<Particle>(numOfCircles);
 
             for (int i = 0; i < numOfCircles; i++)
             {
                 var newCirc = new Particle();
 
-                newCirc.pos = new Vector2(Raylib.GetRandomValue(100, 900), -60 * i);
+                newCirc.pos = new Vector2(Raylib.GetRandomValue(100, 900), -80 * i);
 
                 newCirc.vel = new Vector2(0, 0);
                 newCirc.force = new Vector2(0, 0);
 
-                newCirc.radius = 20;
+                newCirc.radius = 10;
                 newCirc.mass = (float) Math.Pow(newCirc.radius, 2) / 4;
                 newCirc.elasticity = 0.5f;
 
@@ -35,6 +35,11 @@ namespace Radius2D
 
             while (!Raylib.WindowShouldClose())
             {
+                foreach (Particle circ in circles)
+                {
+                    circ.Update();
+                }
+
                 Raylib.BeginDrawing();
                     Raylib.ClearBackground(Color.DARKGRAY);
 
