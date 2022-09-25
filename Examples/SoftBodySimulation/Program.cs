888102b97ebe80e8c7a23aab63760677553ac661
Muhammad Ishaque Junejo <ishaquejunejo33@gmail.com>
2022-09-25 12:46:04 +0500

    Soft Body Simulation Example Added

---
+++ Examples/SoftBodySimulation/Program.cs
@@ -0,0 +1,130 @@
+﻿using Raylib_cs;
+
+// Namespace for the Physics Simulation
+namespace Radius2D
+{
+    // Main Class of the program
+    class Program
+    {
+        // Main function of the Class to be executed
+        static void Main()
+        {
+            // Initializing the Window to be rendered on
+            const int Width = 1050;
+            const int Height = 950;
+            Raylib.InitWindow(Width, Height, "Physics Simulation");
+
+            // Initializing the List of Lines
+            List<Line> lines = new List<Line>(0);
+            var line01 = new Line(-50, 600, Width - 250, Height - 1);
+            lines.Add(line01);
+            var line02 = new Line(1, 0, 1, Height);
+            lines.Add(line02);
+            var line03 = new Line(Width, 0, Width, Height);
+            lines.Add(line03);
+            var line04 = new Line(0, Height, Width, Height);
+            lines.Add(line04);
+            var line05 = new Line(Width / 2, 350, Width, 150);
+            lines.Add(line05);
+            var line06 = new Line(Width / 2, 350, Width, 500);
+            lines.Add(line06);
+
+            // Initializing the List of Balls/Circles
+            int numOfCircs = 10;
+            float radie = 50;
+            List<Circle> circles = new List<Circle>(0);
+            var centerOfCirc = new Circle(700, 150, 0, 0, 15, 20, 0.8f, Color.GRAY);
+            circles.Add(centerOfCirc);
+            for (float i = 0; i < 360; i += 360 / numOfCircs)
+            {
+                var newCirc = new Circle(700 + (float)((Math.Cos(i * Math.PI / 180)) * radie), 150 + (float)((Math.Sin(i * Math.PI / 180)) * radie), 0, 0, 10, 20, 0.8f, Color.WHITE);
+                circles.Add(newCirc);
+            }
+
+            // Initializing the List of Spring connected to Balls/Circles
+            List<Spring> links = new List<Spring>(0);
+            for (int i = 0; i < numOfCircs; i++)
+            {
+                var newLink = new Spring(circles[0], circles[i + 1], radie, 15.0f, 5.0f);
+                links.Add(newLink);
+                if (i != 0)
+                {
+                    var internalLink = new Spring(circles[i], circles[i + 1], (radie * 2.0f * (float) Math.PI) / numOfCircs, 15.0f, 5.0f);
+                    links.Add(internalLink);
+                }
+            }
+            var extraLink = new Spring(circles[1], circles[numOfCircs], (radie * 2.0f * (float) Math.PI) / numOfCircs, 15.0f, 5.0f);
+            links.Add(extraLink);
+
+            // Some Extra Variables
+            float FPS;
+            float deltaTime;
+            string fpsText;
+            int subSteps = 2;
+
+            // Setting FrameRate and Starting the Main Loop
+            //Raylib.SetTargetFPS(120);
+            while (!Raylib.WindowShouldClose())
+            {
+                // Updating the Extra Variables
+                FPS = Raylib.GetFPS();
+                fpsText = Convert.ToString(FPS);
+                deltaTime = Raylib.GetFrameTime();
+
+                for (int i = 0; i < subSteps; i++)
+                {
+                    // Iterating through the List of Balls/Circles
+                    foreach (Circle circle in circles)
+                    {
+                        // Updating the Positions of Balls/Circles
+                        circle.Update(Width, Height, deltaTime / subSteps);
+                        // Looking for and Resolving the Collision between the Balls/Circles
+                        foreach (Circle circ in circles)
+                        {
+                            circle.CollisionResponseCircle(circ, deltaTime / subSteps);
+                        }
+                        foreach (Line l in lines)
+                        {
+                            circle.CollisionResponseLine(l, deltaTime / subSteps);
+                        }
+                    };
+                }
+                
+                foreach (Spring link in links)
+                {
+                    link.update(deltaTime / subSteps);
+                }
+                
+                // Rendering Section of the Program
+                Raylib.BeginDrawing();
+                    // Drawing Background
+                    Raylib.ClearBackground(Color.DARKGRAY);
+
+                    // Draw the current FrameRate
+                    Raylib.DrawText(fpsText, 20, 20, 20, Color.RAYWHITE);
+
+                    foreach (Spring link in links)
+                    {
+                        link.draw();
+                    }
+
+                    // Drawing every Ball/Circle
+                    foreach (Circle circ in circles)
+                    {
+                        circ.Draw();
+                    }
+                    // Drawing every Line
+                    foreach (Line l in lines)
+                    {
+                        l.DrawLine();
+                    }
+                
+                // End of Rendering section
+                Raylib.EndDrawing();
+            }
+
+            // Closing/Ending the Program
+            Raylib.CloseWindow();
+        }
+    }
+}
---
+++ Examples/SoftBodySimulation/Spring.cs
@@ -0,0 +1,67 @@
+using System.Numerics;
+using Raylib_cs;
+
+namespace Radius2D
+{
+    // Class for the Spring Connections
+    public class Spring
+    {
+        // Few Private Variables
+        private Circle ball1;
+        private Circle ball2;
+        private float length;
+        private float stiffness;
+        private float dampFactor;
+
+        // Constructor
+        public Spring(Circle circ1, Circle circ2, float length, float stiff, float damp)
+        {
+            this.ball1 = circ1;
+            this.ball2 = circ2;
+
+            if (circ1.radius + circ2.radius <= length)
+            {
+                this.length = length;
+            }else
+            {
+                this.length = circ1.radius + circ2.radius;
+            }
+            this.stiffness = stiff;
+            this.dampFactor = damp;
+            
+        }
+        
+        // Update Function to update Springs
+        public void update(float deltaTime)
+        {
+            // Finding the Normalized Position vector
+            Vector2 len = this.ball1.pos - this.ball2.pos;
+            float distance = (float) Math.Sqrt(len.X * len.X + len.Y * len.Y);
+            Vector2 normal = len / distance;
+
+            // Difference in length
+            float deltaLength = distance - this.length;
+
+            // Finding the velocity difference
+            Vector2 deltaVel = this.ball1.vel - this.ball2.vel;
+
+            // Dot product of velocity difference and normal position vector
+            float dampFactorDotProduct = Vector2.Dot(deltaVel, normal);
+
+            // Forces calculation
+            float springForce = deltaLength * this.stiffness;
+            Vector2 dampForce = (deltaVel) * dampFactor;
+            Vector2 totalForce = (springForce * normal) + dampForce;
+
+            // Adding the forces
+            this.ball1.force -= totalForce;
+            this.ball2.force += totalForce;
+        }
+
+        // Function to Draw Springs (as Lines)
+        public void draw()
+        {
+            Raylib.DrawLineV(ball1.pos, ball2.pos, Color.WHITE);
+        }
+    }
+}
